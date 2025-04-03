8699c5bc74ffc03b439f17065da8e3d3c6995cf3
Yashavantha P <yashavanthap.btech23@gmail.com>
2025-04-03 21:56:30 +0530

    deleted

--- test_main.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from main import add
-
-class TestAdd(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-
-if __name__ == "__main__":
-    unittest.main()
